1633290e84b615be5a373e85c4e42ef3e2d8b5fd
Matthew Smith <m@lattejed.com>
2022-03-03 12:44:33 +0700

    Refactoring

--- src/components/network/Network.tsx
+++ src/components/network/Network.tsx
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react';
 import { ApplicationModal, useModalOpen, useToggleModal } from '../../state/application';
-import useOnClickOutside from '../../hooks/useOutsideClick';
+import useOutsideClick from '../../hooks/useOutsideClick';
 
 export default function Network(): JSX.Element {
   const node = useRef<HTMLDivElement>();
   const open = useModalOpen(ApplicationModal.NETWORK_SELECTOR);
   const toggle = useToggleModal(ApplicationModal.NETWORK_SELECTOR);
 
-  useOnClickOutside(node, toggle);
+  useOutsideClick(node, toggle);
 
   return (
     <div ref={node as never} className="">
