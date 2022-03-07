5e7ad0d70e460301559ece7f268a454089269895
Matthew Smith <m@lattejed.com>
2022-03-07 12:57:10 +0700

    Get rid of input focus

--- src/components/TokenInput.tsx
+++ src/components/TokenInput.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 export default function TokenInput(): JSX.Element {
   const [value, setValue] = useState('1.000000000000000000');
 
   return (
-    <div className="p-5 border rounded-2xl">
+    <div className="w-full p-5 border rounded-2xl">
       <div className="">
         <div className="">
-          <input className="w-full" type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+          <input
+            className="w-full border-0 focus:ring-0"
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
           <div>ETH</div>
         </div>
         <div>Balance</div>
