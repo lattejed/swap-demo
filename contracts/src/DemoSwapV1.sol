51833e313f0f623ea672acc1229f099ba504de20
Matthew Smith <m@lattejed.com>
2022-03-16 16:47:45 +0700

    Added contracts

---
+++ contracts/src/DemoSwapV1.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.10;
+
+/// @title DemoSwapV1
+/// @author Matthew Smith <m@lattejed.com>
+contract DemoSwapV1 {
+  constructor() {}
+}
