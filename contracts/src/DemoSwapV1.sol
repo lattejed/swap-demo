b4f4dfc0faedfd3e9c259683ce2acfe2640ba3d7
Matthew Smith <m@lattejed.com>
2022-03-17 13:22:42 +0700

    Use new forge-template

--- contracts/src/DemoSwapV1.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
-
-/// @title DemoSwapV1
-/// @author Matthew Smith <m@lattejed.com>
-contract DemoSwapV1 {
-  constructor() {}
-}
--- contracts/src/test/utils/DSTestPlus.sol
+++ contracts/src/test/utils/DSTestPlus.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.10;
 
-import { DSTest } from "@ds/test.sol";
-import { ERC20 } from "@solmate/tokens/ERC20.sol";
-import { stdCheats, stdError } from "@std/stdlib.sol";
-import { Vm } from "@std/Vm.sol";
+import { DSTest } from "ds-test/test.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { stdCheats, stdError } from "forge-std/stdlib.sol";
+import { Vm } from "forge-std/Vm.sol";
 
 contract DSTestPlus is DSTest, stdCheats {
   /// @dev Use forge-std Vm logic
-  // solhint-disable-next-line const-name-snakecase
   Vm public constant vm = Vm(HEVM_ADDRESS);
 
   function _assertERC20Eq(ERC20 erc1, ERC20 erc2) internal {
