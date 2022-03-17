51c09278e0036f5dee9adab03f66cfbf026b8125
Matthew Smith <m@lattejed.com>
2022-03-17 13:52:23 +0700

    Update NatSpec comments

--- contracts/src/DemoSwapV1.sol
+++ contracts/src/DemoSwapV1.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.10;
 
 /// @title DemoSwapV1
 /// @author Matthew Wiriyathananon-Smith <m@lattejed.com>
+/// @notice This is a contract similar to Uniswap V1 developed from first principles. It is not an attempt to recreate Uniswap V1 in its entirety or guess what the actual impolementation looks like.
+/// @dev This is meant to be a learning exercise for the author. Do not use this in production.
 contract DemoSwapV1 {
   constructor() {}
 }
