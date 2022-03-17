e68974b05c98dc7226623b2848406dfb4ba93c38
Matthew Smith <m@lattejed.com>
2022-03-17 14:13:59 +0700

    Update NatSpec comments

--- contracts/src/DemoSwapV1.sol
+++ contracts/src/DemoSwapV1.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.10;
 
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+
 /// @title DemoSwapV1
 /// @author Matthew Wiriyathananon-Smith <m@lattejed.com>
 /// @notice This is a contract similar to Uniswap V1 developed from first principles.
 /// @notice It is not an attempt to recreate Uniswap V1 in its entirety or guess what
-/// @notice the actual implementation looks like.
+/// @notice the actual implementation looks like. This is meant to work for the stated
+/// @notice purpose while being 1) correct and 2) secure. This contains no optimizations.
 /// @dev This is meant to be a learning exercise for the author. Do not use this in production.
 contract DemoSwapV1 {
   constructor() {}
