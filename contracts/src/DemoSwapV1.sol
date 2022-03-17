0b4cbd55ba2dbe5fadcba561e8bab5ed1fb8d48a
Matthew Smith <m@lattejed.com>
2022-03-17 13:43:57 +0700

    Added DemoSwapV1

---
+++ contracts/src/DemoSwapV1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.10;
+
+/// @title DemoSwapV1
+/// @author Matthew Wiriyathananon-Smith <m@lattejed.com>
+contract DemoSwapV1 {
+  string private _gm;
+
+  constructor() {}
+}
---
+++ contracts/src/test/DemoSwapV1.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.10;
+
+import { DSTestPlus } from "./utils/DSTestPlus.sol";
+import { DemoSwapV1 } from "../DemoSwapV1.sol";
+
+contract DemoSwapV1Test is DSTestPlus {
+  DemoSwapV1 private _demoSwap;
+
+  function setUp() public {
+    _demoSwap = new DemoSwapV1();
+  }
+
+  function test_() public {}
+}
