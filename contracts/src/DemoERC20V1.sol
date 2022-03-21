fd5078e60ff681c947d997ed32469bbc9ec589f5
Matthew Smith <m@lattejed.com>
2022-03-21 17:04:24 +0700

    Add mint method

--- contracts/src/DemoERC20V1.sol
+++ contracts/src/DemoERC20V1.sol
@@ -14,4 +14,8 @@ contract DemoERC20V1 is ERC20 {
     string memory _symbol,
     uint8 _decimals
   ) ERC20(_name, _symbol, _decimals) {}
+
+  function mint(address to, uint256 value) public virtual {
+    _mint(to, value);
+  }
 }
