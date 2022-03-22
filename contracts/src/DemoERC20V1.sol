e9636bc32fa3cf4bb44b560d032c1216020b5b78
Matthew Smith <m@lattejed.com>
2022-03-22 09:41:32 +0700

    Add initial mint and test

--- contracts/src/DemoERC20V1.sol
+++ contracts/src/DemoERC20V1.sol
@@ -18,9 +18,11 @@ contract DemoERC20V1 is ERC20 {
         string memory _name,
         string memory _symbol,
         uint8 _decimals,
-        address payable owner_
+        address payable owner_,
+        uint256 _initialSupply
     ) ERC20(_name, _symbol, _decimals) {
         _owner = owner_;
+        _mint(owner_, _initialSupply);
     }
 
     function mint(address to, uint256 value) public virtual {
