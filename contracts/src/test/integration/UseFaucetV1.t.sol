51cc8ff8d12631a9b5b3ef908fac0ea1e3372a18
Matthew Smith <m@lattejed.com>
2022-03-22 12:06:37 +0700

    Removed integration tests

--- contracts/src/test/integration/UseFaucetV1.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
-
-import {DSTestPlus} from "../utils/DSTestPlus.sol";
-
-contract UseFaucetV1Test is DSTestPlus {
-    function setUp() public {}
-}
