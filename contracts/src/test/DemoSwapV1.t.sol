26b9dbc869839602cce21ad42bccdceb9bf42c33
Matthew Smith <m@lattejed.com>
2022-03-24 21:51:56 +0700

    Refactored error signatures and added tests

--- contracts/src/test/DemoSwapV1.t.sol
+++ contracts/src/test/DemoSwapV1.t.sol
@@ -49,6 +49,7 @@ contract DemoSwapV1Test is DSTestPlus {
         // We *will* get overflows for very large amounts so let's limit them
         VM.assume(_tokenAAmt <= 1e12 * 1e18);
         VM.assume(_tokenBAmt <= 1e12 * 1e18);
+
         _deposit(_tokenAAmt, _tokenBAmt);
         assertEq(
             _swap.lpToken().balanceOf(_user),
@@ -59,31 +60,47 @@ contract DemoSwapV1Test is DSTestPlus {
     function testWithdraw() public {
         _deposit(1e12 * 1e18, 1e12 * 1e18);
         VM.prank(_user);
-        _swap.withdraw(1e12 * 1e18);
+        _swap.withdraw(FixedPointMathLib.sqrt(1e12 * 1e18 * 1e12 * 1e18));
         assertEq(_swap.lpToken().balanceOf(_user), 0);
         assertEq(_tokenA.balanceOf(_user), 1e12 * 1e18);
         assertEq(_tokenB.balanceOf(_user), 1e12 * 1e18);
     }
 
     function testWithdrawZero() public {
-        VM.expectRevert(bytes("")); // FixedPointMathLib.divWadUp revert
+        VM.expectRevert(ERROR_NULL);
         VM.prank(_user);
         _swap.withdraw(0);
     }
 
+    function testWithdrawOne() public {
+        _deposit(1, 1);
+        VM.prank(_user);
+        _swap.withdraw(FixedPointMathLib.sqrt(1 * 1));
+        assertEq(_swap.lpToken().balanceOf(_user), 0);
+        assertEq(_tokenA.balanceOf(_user), 1e12 * 1e18);
+        assertEq(_tokenB.balanceOf(_user), 1e12 * 1e18);
+    }
+
     function testWithdrawFuzz(uint256 _tokenAAmt, uint256 _tokenBAmt) public {
-        // testWithdrawZero handles a zero withdrawal
-        VM.assume(_tokenAAmt > 0);
-        VM.assume(_tokenBAmt > 0);
         // We *will* get overflows for very large amounts so let's limit them
         VM.assume(_tokenAAmt <= 1e12 * 1e18);
         VM.assume(_tokenBAmt <= 1e12 * 1e18);
+        // testWithdrawZero handles a zero withdrawal
+        VM.assume(_tokenAAmt > 0 && _tokenBAmt > 0);
+
         _deposit(_tokenAAmt, _tokenBAmt);
         VM.prank(_user);
-        _swap.withdraw(_tokenAAmt * _tokenBAmt);
+        _swap.withdraw(FixedPointMathLib.sqrt(_tokenAAmt * _tokenBAmt));
         assertEq(_swap.lpToken().balanceOf(_user), 0);
-        assertEq(_tokenA.balanceOf(_user), _tokenAAmt);
-        assertEq(_tokenB.balanceOf(_user), _tokenBAmt);
+        assertEq(_tokenA.balanceOf(_user), 1e12 * 1e18);
+        assertEq(_tokenB.balanceOf(_user), 1e12 * 1e18);
+    }
+
+    function testWithdrawTooMuch() public {
+        _deposit(1e12 * 1e18, 1e12 * 1e18);
+        VM.expectRevert(ERROR_UNDER_OVERFLOW);
+        VM.prank(_user);
+        _swap.withdraw(FixedPointMathLib.sqrt(1e12 * 1e18 * 1e12 * 1e18) + 1);
     }
 
     function _deposit(uint256 _tokenAAmt, uint256 _tokenBAmt) private {
